d46c9788ad9168091abc72963c016cf4742d94d4
Rachel Tirkey <13rtirkey@gmail.com>
2024-03-30 08:50:41 +0530

    added code for video stream recorder

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let variable = document.querySelector("video");
+let video = document.querySelector("video");
+let recordBtnCont = document.querySelector(".record-btn-cont");
+let recordBtn = document.querySelector(".record-btn");
+let captureBtnCont = document.querySelector(".capture-btn-cont");
+let captureBtn = document.querySelector(".capture-btn");
+let recordFlag = false;
+
+let recorder;
 
 let constraints = {
     video: true,
@@ -9,4 +16,21 @@ let constraints = {
 navigator.mediaDevices.getUserMedia(constraints)
 .then((stream) => {
    video.srcObject = stream;
+
+   recorder = new MediaRecorder(stream);
+})
+
+recordBtnCont.addEventListener("click", (e) => {
+   if (!recorder) return;
+
+  recordFlag = !recordFlag;
+
+    if (recordFlag) { //strat
+         recorder.start();
+         recordBtn.classList.add("scale-record");
+    }
+    else { // stop
+         recorder.stop();
+         recordBtn.classList.remove("scale-record");
+    }
 })

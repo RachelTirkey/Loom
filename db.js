96f6e4654466e1b820bc6e76d9bf5bcd77f98066
Rachel Tirkey <13rtirkey@gmail.com>
2024-05-03 16:58:06 +0530

    added database access and created objectstore

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
+// Open a database
+// Create objectstore
+// Make Transaction
 let db;
-let openRequest = indexedDB.open("myDataBase", 3);
+let openRequest = indexedDB.open("myDataBase");
 openRequest.addEventListener("success", (e) => {
     console.log("DB Success");
+    db = openRequest.result;
 })
 openRequest.addEventListener("error", (e) => {
     console.log("DB Error");
 })
 openRequest.addEventListener("upgradeneeded", (e) => {
-    console.log("DB upgraded");
+    console.log("DB  and also for initial DB creation");
+    db = openRequest.result;
+
+    console.log(db);
+
+    db.createObjectStore("video", { keyPath: "id"});
+    db.createObjectStore("image", { keyPath: "id"});
 })
